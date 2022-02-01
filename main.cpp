31d2ff61e1d245f6ac2859a80064c725009384f9
George Golubev <george.gol003@gmail.com>
2022-02-01 12:44:43 +0300

    Переподключил либы

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include "stack/stack.h"
+// Наши либы
+#include "stack.h"
+#include "variable_tree.h"
+
+// 
 #include "stdio.h"
 #include "stdlib.h"
-#include "variable_tree/variable_tree.h"
 
 int main(int argc, char const *argv[])
 {
@@ -12,7 +15,7 @@ int main(int argc, char const *argv[])
     AddELement2Stack(&stack, "300");
     AddELement2Stack(&stack, "100");
     PrintStack(&stack);
-    //test_string
+
     return 0;
     // lol
 }
