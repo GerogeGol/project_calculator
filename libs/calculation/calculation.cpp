ccdbaaba365f2f9a40bd82a15e32850d745020ae
yergoff <ruslanja1@mail.ru>
2022-02-03 16:59:30 +0300

    calculation from notation_first

--- libs/calculation/calculation.cpp
+++ libs/calculation/calculation.cpp
@@ -1,5 +1,71 @@
-//
-// Created by rusla on 03.02.2022.
-//
-
 #include "calculation.h"
+
+#include "string.h"
+#include "stdlib.h"
+
+// + - ^ * / ~
+
+double Plus(double num1, num2) {
+    return num1 + num2;
+}
+
+double Minus(double num1, num2) {
+    return num1 - num2;
+}
+
+double Multiple(double num1, num2) {
+    return num1 * num2;
+}
+
+double Division(double num1, num2) {
+    return num1 / num2;
+}
+
+double unarMinus(double x) {
+    return -x;
+}
+
+double Pow(double num1, num2) {
+    double res = 0;
+    for (int i = 0; i < num2; i++) {
+        res *= num1;
+    }
+    return res;
+}
+
+
+int operation_choosen(const char* var) {
+    char operations[] = {"+", "-", "*", "^", "/", "%"};
+    for (int i = 0; i < 6; i++) {
+        if (!strcmp(var, operations[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void Calculation(Stack* stack) {
+    char operations[] = {"+", "-", "*", "^", "/", "%"};
+    Stack result;
+    DLNode *node = stack->start;
+
+    while (node) {
+        char input[20] = { 0 };
+        int i = operation_choosen(input);
+        if (i == -1) {
+            AddELement2Stack(&result, strtod(input, node->item))
+        }
+        else {
+            char number_one[20] = {0};
+            PopElementStack(&result, number_one);
+            double number1;
+            strtod(number_one, number1);
+            char number_two[20] = {0};
+            PopElementStack(&result, number_two);
+            double number2;
+            strtod(number_two, number2);
+        }
+
+        node = node->next;
+    }
+}
--- libs/calculation/calculation.h
+++ libs/calculation/calculation.h
@@ -5,4 +5,15 @@
 #ifndef CALCULATOR_CALCULATION_H
 #define CALCULATOR_CALCULATION_H
 
+#include "../notation/notation.h"
+
+double Plus(double num1, num2);
+double Minus(double num1, num2);
+double Multiple(double num1, num2);
+double Division(double num1, num2);
+double unarMinus(double x);
+double Pow(double num1, num2);
+int operation_choosen(const char* var);
+void Calculation(Stack* stack);
+
 #endif //CALCULATOR_CALCULATION_H
