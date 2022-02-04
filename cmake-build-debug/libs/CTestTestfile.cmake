d2ef2aa173d07affc825de97b63679a359164a0d
George Golubev <34991190+GerogeGol@users.noreply.github.com>
2022-02-04 15:07:56 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/libs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/calc/project_calculator/libs
-# Build directory: D:/projects/calc/project_calculator/cmake-build-debug/libs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
