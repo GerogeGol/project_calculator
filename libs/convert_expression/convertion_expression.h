d2949d045fbcab3a6257172bf5ae5d2ef9a44b94
Ilya <ikrav2003@mail.ru>
2022-02-05 17:46:25 +0300

    сообщенеи

--- libs/convert_expression/convertion_expression.h
+++ libs/convert_expression/convertion_expression.h
@@ -1,8 +1,22 @@
 //
-// Created by Ilya  on 02.02.2022.
+// Created by ILYA && IGOR AKA 17;44 5;2;2022
 //
 
 #ifndef CALCULATOR_CONVERTION_EXPRESSION_H
 #define CALCULATOR_CONVERTION_EXPRESSION_H
+
+#define SIZE_STRING 1000
+#define SIZE_VAR 100
+
+typedef struct variable {
+    char name[SIZE_VAR];
+    double value;
+} variable;
+
+void formatFirstLine();
+
+char *ReadFromConsole();
+
+char *ReadFromFile();
 
 #endif //CALCULATOR_CONVERTION_EXPRESSION_H
