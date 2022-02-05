bb66fe9227d2e0ba0656a18a88c8610d4cb88e7b
Ilya <ikrav2003@mail.ru>
2022-02-05 22:22:33 +0300

    povar

--- libs/convert_expression/convertion_expression.cpp
+++ libs/convert_expression/convertion_expression.cpp
@@ -2,8 +2,23 @@
 
 #include "stdio.h"
 #include "string.h"
+#include "stdbool.h"
 
-void formatFirstLine(char *buf, char *str) {
+#include "../variable_tree/variable_tree.h"
+
+void format_first_line(char *buf, char *exercise) {
+    int j = 0;
+    for (int i = 0; i < SIZE_STRING; ++i) {
+        if (buf[i] == ' ') {
+            continue;
+        } else {
+            exercise[j] = buf[i];
+            ++j;
+        }
+    }
+}
+
+void format_variable_lines(char *buf, char *str){
     int j = 0;
     for (int i = 0; i < SIZE_STRING; ++i) {
         if (buf[i] == ' ') {
@@ -13,21 +28,28 @@ void formatFirstLine(char *buf, char *str) {
             ++j;
         }
     }
-}
-
-variable FillValues() {
 
 }
 
 char *ReadFromConsole() {
     char buf[SIZE_STRING] = {'\0'};
-    char str[SIZE_STRING] = {'\0'};
+    char exercise[SIZE_STRING] = {'\0'};
 
-    scanf("%s", &buf);
-    formatFirstLine(buf, str);
+    bool scan_first_line = true;
+    while (gets(buf)){
+        if (scan_first_line){
+            format_first_line(buf, exercise);
+            scan_first_line = false;
+        }
+        else{
+            char str[SIZE_STRING] = {'\0'};
+            format_variable_lines(buf, str);
+        }
+
+    }
 }
 
-char *ReadFromFile() {
+/*char *ReadFromFile() {
     FILE *fr;
     fr = fopen("input.txt", "rt");
 
@@ -35,4 +57,4 @@ char *ReadFromFile() {
     while (fgets(buf, SIZE_STRING, fr)) {
 
     }
-}
+}*/
