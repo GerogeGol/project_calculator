82ab9ede97fdee17d32c2b6e993297989c970516
George Golubev <george.gol003@gmail.com>
2022-02-05 13:47:19 +0300

    working without variables

--- libs/calculation/calculation.cpp
+++ libs/calculation/calculation.cpp
@@ -1,35 +1,41 @@
 #include "calculation.h"
 
-#include "string.h"
 #include "stdlib.h"
+#include "string.h"
 
 // функция сложения двух чисел
-double Plus(double num1, double num2) {
+double Plus(double num1, double num2)
+{
     return num1 + num2;
 }
 
 // функция вычитания двух чисел
-double Minus(double num1, double num2) {
+double Minus(double num1, double num2)
+{
     return num2 - num1;
 }
 
 // функция умножения двух чисел
-double Multiple(double num1, double num2) {
+double Multiple(double num1, double num2)
+{
     return num1 * num2;
 }
 
 // функция деления двух чисел
-double Division(double num1, double num2) {
+double Division(double num1, double num2)
+{
     return num2 / num1;
 }
 
 // функция унарного минуса
-double unarMinus(double x) {
+double unarMinus(double x)
+{
     return -x;
 }
 
 // функция возведения в степень
-double Pow(double num1, double num2) {
+double Pow(double num1, double num2)
+{
     double res = 0;
     for (int i = 0; i < num1; i++) {
         res *= num2;
@@ -38,69 +44,64 @@ double Pow(double num1, double num2) {
 }
 
 // для реализации виртуальной функции
-int operation_choosen(const char* var) {
-    char operations[] = {'+', '-', '*', '/', '^'};
+int operation_choosen(const char* var)
+{
+    char operations[5][100] = {"+", "-", "*", "/", "^"};
     for (int i = 0; i < 5; i++) {
-        if (!strcmp(var, &operations[i])) {
+        if (!strcmp(var, operations[i])) {
             return i;
         }
     }
     return -1;
 }
 
-double Operation(double num1, double num2, double (*pred)(double, double)) {
+double Operation(double num1, double num2, double (*pred)(double, double))
+{
     return pred(num1, num2);
 }
 
-void Calculation(Stack* stack) {
-    Stack result; // создаем стек, чтобы записывать туда цифры и в последующем результат
-    DLNode *node = stack->start; // начинаем идти с начала полученного стека
+void Calculation(Stack* stack)
+{
+    NumericStack result;          // создаем стек, чтобы записывать туда цифры и в последующем результат
+    DLNode* node = stack->start;  // начинаем идти с начала полученного стека
     // для виртуальной функции
     double (*funcs[])(double, double) = {
-            Plus,
-            Minus,
-            Multiple,
-            Division,
-            Pow };
+        Plus,
+        Minus,
+        Multiple,
+        Division,
+        Pow};
 
     // пока не достигнем конца в полученном стеке польской нотации
     while (node) {
-        //char input[30] = node->item; // берем значение из стека
-        char *ptr; // указатель на поинтер для функции strtod
-        int i = operation_choosen(node->item); // для работы виртуальной функции
+        // char input[30] = node->item; // берем значение из стека
+        char* ptr;                              // указатель на поинтер для функции strtod
+        int i = operation_choosen(node->item);  // для работы виртуальной функции
         // проверка на запись вида "-52"
         if (node->item[0] == '-' && node->item[1] >= '0') {
             int count_for_num = 0;
-            char num[20] = {0}; // если у числа стоит впереди минус, то проводим над ним UnarMinus
+            char num[20] = {0};  // если у числа стоит впереди минус, то проводим над ним UnarMinus
             for (int j = 1; j < strlen(node->item); j++) {
                 num[count_for_num++] = node->item[j];
             }
-            double toUnarMinusResult; // записываем рез в эту переменную, затем добавляем в стек result
+            double toUnarMinusResult;  // записываем рез в эту переменную, затем добавляем в стек result
             toUnarMinusResult = strtod(num, &ptr);
             AddELement2NumStack(&result, unarMinus(toUnarMinusResult));
-            node = node->next; // переход к следующему элементу в обратной польской нотации
+            node = node->next;  // переход к следующему элементу в обратной польской нотации
             continue;
-        }
-        else {
-            if (i == -1) { // если функция оператора возвращает минус один, значит в инпуте число
+        } else {
+            if (i == -1) {  // если функция оператора возвращает минус один, значит в инпуте число
                 AddELement2NumStack(&result, strtod(node->item, &ptr));
-            }
-            else { // если встретили знак, достаем два числа из стека
-                char number_one[20] = {0};
-                PopElementNumStack(&result);
-                double number1 = strtod(number_one, &ptr);
-                char number_two[20] = {0};
-                PopElementNumStack(&result);
-                double number2 = strtod(number_two, &ptr);
-                double after_op = Operation(number1, number2, funcs[i]); // выполняем операцию
-                AddELement2NumStack(&result, after_op); // записываем в стек result
+            } else {  // если встретили знак, достаем два числа из стека
+                double number1 = PopElementNumStack(&result);
+                double number2 = PopElementNumStack(&result);
+                double after_op = Operation(number1, number2, funcs[i]);  // выполняем операцию
+                AddELement2NumStack(&result, after_op);                   // записываем в стек result
             }
         }
 
-        node = node->next; // переход к следующему элементу в обратной польской нотации
+        node = node->next;  // переход к следующему элементу в обратной польской нотации
     }
 
-    PrintStack(&result); // печать результата
+    PrintNumStack(&result);  // печать результата
 }
-
-
--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[])
 
     PrintStack(&stack);
     printf("%d\n", StackIsEmpty(&stack));
-    char expression[N] = "(abc+xxx*2)/(z^3*(asd-50))";
+    char expression[N] = "2+2";
 
     Stack stack_expression = to_notation(expression);
     PrintStack(&stack_expression);
